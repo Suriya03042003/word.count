eed9da7ee8daa0ebb381321ee2ac9b698c1de200
Suriya03042003 <118451124+Suriya03042003@users.noreply.github.com>
2023-08-06 22:20:02 +0530

    Add files via upload

---
+++ React Learning/Loginpage/favicon/Car.js
@@ -0,0 +1,16 @@
+import Garage from './Garage'
+
+function Car(){
+
+    const fruits = {name:"apple",colour:"red"}
+    return(
+
+           <div> 
+            <h1>hello</h1>
+            <Garage fruits={fruits}/> 
+            </div>
+    )
+    
+}
+
+export default Car;
