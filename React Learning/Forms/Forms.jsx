e4318b7fafec8738d8d12762b4cf420dfa867659
Suriya03042003 <118451124+Suriya03042003@users.noreply.github.com>
2023-08-24 19:34:31 +0530

    Add files via upload

--- React Learning/Forms/Forms.jsx
+++ React Learning/Forms/Forms.jsx
@@ -3,32 +3,47 @@ import { useState } from 'react';
 import './Forms.css';
 
 function Forms(){
-    const [inputs,setInputs] = useState({})
-    function updateForm(e){
-        e.preventDefault();
-        console.log(inputs)
+    const [inputs,setInputs] = useState({phone:"+91 "}) 
+    
+      function handler(e){
+      const name = e.target.name;
+      const value = e.target.value;
+      setInputs((previousState)=> { return {...previousState,[name]:value}})
     }
+    
+
+    function SubmitHandler(e){
+      e.preventDefault();
+      console.log(inputs);
+    }
+
+
     return(
     <>
      <div className="center"> 
       <h1>Login</h1>
-      <form method="post">
+      <form method="post" onSubmit={SubmitHandler}>
         <div className="txt_field">
-          <input type="text" required onChange={(e)=> {setInputs((pre)=> {return {...pre, name: e.target.value}})}}></input>
+          <input type="text" name="name" required onChange={handler}></input>
           <span></span>
           <label>Username</label>
         </div>
         <div className="txt_field">
-          <input type="password" required onChange={(e)=> {setInputs((pre)=> {return {...pre, password: e.target.value}})}}></input>
+          <input type="password" name="password" required onChange={handler}></input>
           <span></span>
           <label>Password</label>
         </div>
         <div className="txt_field">
-          <input type="email" required onChange={(e)=> {setInputs((pre)=> {return {...pre, email: e.target.value}})}}></input>
+          <input type="email" name="email" required onChange={handler}></input>
           <span></span>
           <label>Email</label>
         </div>
-        <button onClick={updateForm}>Submit Form</button>
+        <div className="txt_field">
+          <input type="text" name="phone" required onChange={handler} value={inputs.phone}></input>
+          <span></span>
+          <label>Phone</label>
+        </div>
+        <input className="submit" type="submit" value="Submitform"></input>
       </form>
     </div>
     </>
