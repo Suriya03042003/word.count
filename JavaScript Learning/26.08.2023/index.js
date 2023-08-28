a15ebcbf29f30dec9a9b830a301264b6a2a184ca
Suriya03042003 <118451124+Suriya03042003@users.noreply.github.com>
2023-08-28 18:35:45 +0530

    Add files via upload

--- JavaScript Learning/26.08.2023/index.js
+++ JavaScript Learning/26.08.2023/index.js
@@ -20,8 +20,32 @@ arr.forEach(ele =>
     {
         if(ele > 0) 
         {
-        sum += ele;s
+        sum += ele;
         }
         }
     )
     document.getElementById('Sum').innerHTML  = sum;
+console.log(arr);
+
+// array map
+console.log(arr.map(ele => ele+sum) );   
+
+//arrat filter
+
+console.log(arr.filter(ele => ele>0))
+
+// array reducer
+
+cost = [100,200,300,400,500,600];
+console.log(cost.reduce((total,a)=> a+total));
+
+// Letter Occurance
+
+array = [
+    ['a','b','c'],
+    ['d','e','a'],
+    ['b','f','g'],
+
+];
+
+console.log(array.flat());
